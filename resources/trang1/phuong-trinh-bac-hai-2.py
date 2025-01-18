d724709bd3f2cbe68ba743362e8fd77a816fde21
tuandaynek <103641348+tuandaynek@users.noreply.github.com>
2025-01-18 13:41:55 +0700

    Update phuong-trinh-bac-hai-2.py

--- resources/trang1/phuong-trinh-bac-hai-2.py
+++ resources/trang1/phuong-trinh-bac-hai-2.py
@@ -1,13 +1,20 @@
-import math
+import math #thư viện dùng để sử dụng để tìm căn n trong bài toán này
 
 a, b, c = map(float, input().split())
 delta = (b**2)-4*a*c
 if(delta == 0):
     x = -b/(2*a)
-    print(f"{x:.2f}")
+    print(f"{x:.2f}") #làm tròn 2 chữ số thập phân
 elif(delta > 0):
     x1 = (-b+math.sqrt(delta))/(2*a)
     x2 = (-b-math.sqrt(delta))/(2*a)
     print(f"{(max(x1, x2)):.2f}\n{(min(x1, x2)):.2f}")
 else:
     print(-1)
+
+"""
+Các mấu chốt để giải được bài tập này::
+- kiến thức về thư viện bên ngoài (import math)
+- cách làm tròn số thập phân (x:.2f)
+- nhập các số trên cùng 1 dòng (split())
+"""
